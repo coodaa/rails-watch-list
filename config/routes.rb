182487b0fd3f3549c369307f87e98cd8e503e588
Florian Schneider schneider.f@me.com <schneider.f@me.com>
2022-05-19 16:53:58 +0200

    Prepare rails app with external specs

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   # root "articles#index"
+  root to: "movies#index"
+  resources :movies, only: [:index, :show, :new] do
+    resources :list, only: [:create] do
+    resources :booksmarks, only: [create]
+    end
+  end
 end
