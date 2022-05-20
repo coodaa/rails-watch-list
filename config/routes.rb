caefd2416f6a5beec1d2358a3f96877012898404
Florian Schneider schneider.f@me.com <schneider.f@me.com>
2022-05-20 12:44:48 +0200

    copied website

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  # Defines the root path route ("/")
-  # root "articles#index"
-  root to: "movies#index"
-  resources :movies, only: [:index, :show, :new] do
-    resources :list, only: [:create] do
-    resources :booksmarks, only: [create]
-    end
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'lists#index'
+  resources :lists, except: [:edit, :update] do
+    resources :bookmarks, only: :create
+    resources :reviews, only: :create
   end
+  resources :bookmarks, only: :destroy
+  resources :reviews, only: :destroy
 end
